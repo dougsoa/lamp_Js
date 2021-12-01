1d9654e3854291612d0e2fb1b66cca39a5f1d84f
Douglas Oliveira <83471539+dougsoa@users.noreply.github.com>
2021-12-01 15:24:26 -0300

    Update lampada.js

--- lampada.js
+++ lampada.js
@@ -2,13 +2,28 @@ const turnOn = document.getElementById('turnOn')
 const turnOff = document.getElementById('turnOff')
 const lamp = document.getElementById('lamp')
 
+function isLampBroken() {
+  return lamp.src.indexOf('quebrada') > -1
+}
+
 function lampOn() {
-  lamp.src = './img/Ligada.png'
+  if (!isLampBroken()) {
+    lamp.src = './img/Ligada.png'
+  }
 }
 
 function lampOff() {
-  lamp.src = './img/Desligada.png'
+  if (!isLampBroken()) {
+    lamp.src = './img/Desligada.png'
+  }
+}
+
+function lampBroken() {
+  lamp.src = './img/Quebrada.jpg'
 }
 
 turnOn.addEventListener('click', lampOn)
 turnOff.addEventListener('click', lampOff)
+lamp.addEventListener('mouseover', lampOn)
+lamp.addEventListener('mouseleave', lampOff)
+lamp.addEventListener('dblclick', lampBroken)
